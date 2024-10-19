909ce8d27554ad74bb2e048a9e9b252e76dd0160
rmCoder7 <mohammedayon2021@gmail.com>
2024-10-20 02:14:50 +0600

    add

--- chapter 3(string)/intro_string.py
+++ chapter 3(string)/intro_string.py
@@ -31,4 +31,38 @@ print(d)
 
 
 
+#chapter 3
+
+
+name = 'Ayon'
+
+# print(len(name))
+
+# print(name.endswith('on'))
+
+# print(name.startswith("Ay"))
+# # it case senstitive
+# print(name.capitalize())
+
+
+counter = "abcdefghij"
+
+result = counter.count('g')
+# print(result)
+
+s = "hello world"
+
+p = s.find("o")
+print(p)
+
+
+
+
+prog = " I am network enginner"
+
+exe = prog.replace("network" , "automation")
+
+print(exe)   
+
+
 
